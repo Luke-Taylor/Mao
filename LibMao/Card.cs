5b121178adc31d2b703ca4a046cd12ab0d62d41e
Luke <contact@luketaylor.rocks>
2017-11-26 17:12:38 +0000

    First commit

---
+++ LibMao/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMao
+{
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
+    }
+
+    public enum CardValue
+    {
+        One = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public class Card
+    {
+        public Card(Suit s, CardValue v)
+        {
+            Suit = s;
+            Value = v;
+        }
+
+        public Suit Suit;
+        public CardValue Value;
+    }
+}
---
+++ LibMao/Deck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMao
+{
+    public class Deck
+    {
+        public List<Card> Cards;
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+            for(int i = 0; i < 4; i++)
+            {
+                for(int c = 1; c < 13; c++)
+                {
+                    Cards.Add(new Card((Suit)i,(CardValue)c));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random r = new Random();
+            for(int i = Cards.Count - 1; i > 0; i--)
+            {
+                int target = r.Next(i + 1);
+                Card tmp = Cards[i];
+                Cards[i] = Cards[target];
+                Cards[target] = tmp;
+            }
+        }
+    }
+}
